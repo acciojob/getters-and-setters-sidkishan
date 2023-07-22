b04604266053f73980170506b858843cd60b892b
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-07-22 23:11:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,14 +30,14 @@ class Teacher extends Person {
 		console.log(this._name + " is teaching")
 	}
 }
-const person1 = new Person("John Doe", 30);
-console.log(`Person's name: ${person1.name}, Age: ${person1.age}`);
+// const person1 = new Person("John Doe", 30);
+// console.log(`Person's name: ${person1.name}, Age: ${person1.age}`);
 
-const student1 = new Student("Alice", 20);
-student1.study();
+// const student1 = new Student("Alice", 20);
+// student1.study();
 
-const teacher1 = new Teacher("Mr. Smith", 45);
-teacher1.teach();
+// const teacher1 = new Teacher("Mr. Smith", 45);
+// teacher1.teach();
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
