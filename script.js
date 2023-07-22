4627285e163bb9438123a742ffbe1ea9467a5281
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-07-22 22:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ class Person {
 	constructor(name,age){
 		this.name = name;
 		this.age = age;
+	}
 		get name(){
 			retrun this.name;
 		}
 		set age(age){
 			this.age = age;
 		}
-	}
+	
 }
 
 class Student extends Person {
